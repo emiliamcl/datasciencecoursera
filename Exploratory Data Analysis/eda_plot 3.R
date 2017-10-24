923334e2c191b4f3a39c61fd910420678708055f
emiliamcl <emiliamcl@users.noreply.github.com>
2017-10-24 10:53:34 +0100

    Update eda_plot 3.R

--- Exploratory Data Analysis/eda_plot 3.R
+++ Exploratory Data Analysis/eda_plot 3.R
@@ -21,6 +21,8 @@ with(subs_plot3,lines(Time,as.numeric(as.character(Sub_metering_1))))
 with(subs_plot3,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
 with(subs_plot3,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-
 # title
 title(main="Energy sub-metering")
+#saving png
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
